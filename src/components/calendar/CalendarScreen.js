403d2b0aa2ea52066c2de9ab29adf6bc07ad1e0d
Matias Salicru <mattiassalicru@gmail.com>
2021-03-29 07:58:04 -0300

    Big calendar added, with moment, and some styles in login screen

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+import { Navbar } from "../ui/Navbar";
+import { messages } from "../../helpers/calendar-messages-es";
+import { CalendarEvent } from "./CalendarEvent";
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import "moment/locale/es";
+moment.locale("es");
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "Cumpleaños del jefe",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgColor: "#fafafa",
+    notes: "Comprar el pastel",
+    user: {
+      _id: "123",
+      name: "Fernando",
+    },
+  },
+];
+
+export const CalendarScreen = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem("lastView") || "month"
+  );
+
+  const onDoubleClick = (e) => {
+    console.log(e);
+  };
+
+  const onSelectEvent = (e) => {
+    console.log(e);
+  };
+
+  const onViewChange = (e) => {
+    setLastView(e);
+    localStorage.setItem("lastView", e);
+  };
+
+  const eventStyleGetter = (event, start, end, selected) => {
+    const style = {
+      backgroundColor: "#367cf7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+
+    return {
+      style,
+    };
+  };
+  return (
+    <div className="calendar-screen">
+      <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        messages={messages}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChange}
+        view={lastView}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent,
+        }}
+      />
+    </div>
+  );
+};
